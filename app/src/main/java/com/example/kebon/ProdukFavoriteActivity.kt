2a11d1b984013b7c423faf3ac302fd7b00518d06
maling17 <kresnaa09@yahoo.com>
2020-04-19 22:08:37 +0700

    Menambahkan layout detail jasa,lacak pesanan,panen,produk fav,riwayat

---
+++ app/src/main/java/com/example/kebon/ProdukFavoriteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kebon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProdukFavoriteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_produk_favorite)
+    }
+}
---
+++ app/src/main/java/com/example/kebon/RiwayatProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kebon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RiwayatProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_riwayat_profile)
+    }
+}
